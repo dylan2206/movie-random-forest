470258f6f33bbf1b180bdcc6a941a3ac069160f0
Aldien Zone <dylan.valentiono@student.umn.ac.id>
2025-12-05 15:44:19 +0700

    fix error

--- train_models.py
+++ train_models.py
@@ -18,10 +18,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-rf = RandomForestRegressor(
-    n_estimators=300,
-    max_depth=20,
-    random_state=42
+rf = RandomForestRegressor(
+    n_estimators=150,
+    max_depth=15,
+    random_state=42
 )
 
 rf.fit(X_train, y_train)
