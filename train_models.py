77e239dff744a956c67e74e0b8fc46ed3e6d6394
dylan2206 <dylan.valentino@student.umn.ac.id>
2025-12-05 16:33:05 +0700

    train_models.py

--- train_models.py
+++ train_models.py
@@ -4,11 +4,18 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
-df = pd.read_csv("movies_clean.csv")
+# Load dataset
+df = pd.read_csv("final_dataset.csv")
+
+df['gross_worldwide'] = df['gross_worldwide'].fillna(0)
+
+# Convert release_date → year
+df['year'] = pd.to_datetime(df['release_date'], errors='coerce').dt.year
+df['year'] = df['year'].fillna(df['year'].median())
 
 # Features & Target
-feature_cols = ['budget_log', 'votes_log', 'rating', 'duration', 'year', 'profit_log']
-target = 'gross_log'
+feature_cols = ['budget', 'votes', 'rating', 'duration', 'year']
+target = 'gross_worldwide'
 
 X = df[feature_cols]
 y = df[target]
@@ -18,14 +25,17 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-rf = RandomForestRegressor(
-    n_estimators=150,
-    max_depth=15,
-    random_state=42
+# Model Random Forest
+rf = RandomForestRegressor(
+    n_estimators=150,
+    max_depth=15,
+    random_state=42
 )
 
+# Train model
 rf.fit(X_train, y_train)
 
+# Save model to .pkl
 pickle.dump(rf, open("random_forest.pkl", "wb"))
 
 print("Model Random Forest berhasil dibuat & disimpan sebagai random_forest.pkl!")
